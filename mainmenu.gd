151f6954510769f7c7427ede622e45c8f2cc2e6e
sejr12 <sejr.kirk@outlook.dk>
2025-04-29 20:23:48 +0200

    Info menu

--- mainmenu.gd
+++ mainmenu.gd
@@ -13,3 +13,7 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://info.tscn")
